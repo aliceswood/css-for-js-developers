f515aeea73cc4b6e128fac6c26a06c0bf9f2d40c
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-07-24 12:48:10 +0100

    exercise 2: refine

--- sole-and-ankle/src/components/Header/Header.js
+++ sole-and-ankle/src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
 		<header>
 			<SuperHeader />
 			<MainHeader>
-				<Logo />
+				<Side>
+					<Logo />
+				</Side>
 				<Nav>
 					<NavLink href="/sale">Sale</NavLink>
 					<NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,6 +23,7 @@ const Header = () => {
 					<NavLink href="/kids">Kids</NavLink>
 					<NavLink href="/collections">Collections</NavLink>
 				</Nav>
+				<Side />
 			</MainHeader>
 		</header>
 	);
@@ -30,19 +33,21 @@ const MainHeader = styled.div`
 	padding: 0 32px;
 	border-bottom: 1px solid ${COLORS.gray[300]};
 	display: flex;
-	flex-direction: row;
 	height: 4.5rem;
 	align-items: center;
+  
+`;
+const Side = styled.div`
+	flex: 1;
 `;
 
 const Nav = styled.nav`
-	display: flex;
-  align-items: center;
-	margin: 0 auto;
+  display: flex;
+  gap: 48px;
+  margin: 0px 48px;
 `;
 
 const NavLink = styled.a`
-	padding: 48px;
 	font-size: 1.125rem;
 	text-transform: uppercase;
 	text-decoration: none;
